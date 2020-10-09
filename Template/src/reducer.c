bfc67dbc662655f8cd57061f60568a49f84ee8a9
camp0609 <35385718+camp0609@users.noreply.github.com>
2020-10-09 12:22:58 -0500

    Update reducer.c

--- Template/src/reducer.c
+++ Template/src/reducer.c
@@ -1,5 +1,5 @@
 #include "reducer.h"
-finalKeyValueDS * root = NULL;
+finalKeyValueDS *root = NULL;
 // create a key value node
 finalKeyValueDS *createFinalKeyValueNode(char *word, int count){
 	finalKeyValueDS *newNode = (finalKeyValueDS *)malloc (sizeof(finalKeyValueDS));
@@ -46,13 +46,14 @@ void reduce(char *key) {
   FILE *f;
   f = fopen(key, "r");
   if(errno == -1) {
-    printf("open %s wrongly", key);
+		printf("failed to open file\n");   
+		printf("open %s wrongly", key);
     return;
   }
-  char *word;
+  char word[50];
   fscanf(f, "%s", word);
   int count = 0;
-  int occurance;
+  int occurance = 1;
   while(fscanf(f,"%d", &occurance)!=0) {
     count ++;
   }
@@ -63,21 +64,23 @@ void reduce(char *key) {
 // write the contents of the final intermediate structure
 // to output/ReduceOut/Reduce_reducerID.txt
 void writeFinalDS(int reducerID){
-  char *str1 = "output/ReduceOut/Reduce_";
-  char *str2 = ".txt";
+	printf("testing\n");
+  char str1[100] = "output/ReduceOut/Reduce_";
+  char str2[] = ".txt";
   char reduceID[33];
   sprintf(reduceID, "%d", reducerID);
+	//printf("%s\n", str1);
   char *reducestring = strcat(str1, strcat(reduceID, str2));
-  //printf("%s", reducestring);
-  FILE * fp;
+  printf("hello\n");
+  FILE *fp;
   fp = fopen(reducestring, "w");
-  finalKeyValueDS * temp = root;
+  finalKeyValueDS *temp = root;
   while(temp!=NULL) {
     fprintf(fp, "%s %d\n", temp->key, temp->value);
     temp = temp->next;
   }
   freeFinalDS(root);
-  fclose(fp);	
+  fclose(fp);
 }
 
 int main(int argc, char *argv[]) {
@@ -86,9 +89,8 @@ int main(int argc, char *argv[]) {
 		printf("Less number of arguments.\n");
 		printf("./reducer reducerID");
 	}
-
 	// ###### DO NOT REMOVE ######
-	// initialize 
+	// initialize
 	int reducerID = strtol(argv[1], NULL, 10);
 
 	// ###### DO NOT REMOVE ######
